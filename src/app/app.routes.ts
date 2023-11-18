dbc8c1bc2cb826e11085c05c6e718947f6c517fc
Yves Molina <yves.molina@epitech.eu>
2023-11-18 12:35:58 +0100

    feat: hero name

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/hero', pathMatch: 'full' },
+  { path: 'hero', component: HeroComponent },
+];
